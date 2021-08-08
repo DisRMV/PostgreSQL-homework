138f81946095bdca22a12a341f9fe330b23c8765
Victoria Kotova <marinaivika@mail.ru>
2021-08-08 05:03:35 +0300

    Additional task v 2

--- Script 3d lesson additional task.sql
+++ Script 3d lesson additional task.sql
@@ -1,10 +1,6 @@
 CREATE TABLE employee (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(30),
-	id_department INT REFERENCES department(id)
-);
-CREATE TABLE department (
-	id SERIAL PRIMARY KEY,
 	department VARCHAR(70),
 	id_manager INT REFERENCES employee(id)
 );
